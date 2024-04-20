eb194e50d9df35189c6b464029e1ad05bc777cac
Chayok7 <157208542+Chayok7@users.noreply.github.com>
2024-04-20 12:32:39 +0300

    Update ping-pong.py
     основа без мяча

---
+++ ping-pong.py
@@ -0,0 +1,50 @@
+from pygame import *
+
+font.init()
+font1 = font.SysFont('Arial', 80)
+win = font1.render('YOU WIN!', True, (255, 255, 255))
+lose = font1.render('YOU LOSE!', True, (180, 0, 0))
+font2 = font.SysFont('Arial', 36)
+
+ball_image = "Ball.png"
+player1_image = "player1.png"
+player2_image = "player2.png"
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+player1 = Player("player1.png", 50, 250, 5, 80, 100, 10)
+player2 = Player("player2.png", 50, 250, 5, 80, 100, 10)
+
+
+
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
